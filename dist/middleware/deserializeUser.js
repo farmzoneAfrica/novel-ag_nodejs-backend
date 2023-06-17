d78a9b5f8c15ab4d53fd13edf9534f07d6025188
ushashir <laleoict@gmail.com>
2023-06-17 03:08:22 +0100

    roles and permisions table created

--- dist/middleware/deserializeUser.js
+++ dist/middleware/deserializeUser.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.adminAuth = exports.auth = void 0;
+exports.adminAuthdel = exports.adminAuth = exports.auth = void 0;
 const lodash_1 = require("lodash");
 const user_service_1 = require("../services/user.service");
 const app_error_1 = __importDefault(require("../utils/app.error"));
@@ -65,7 +65,6 @@ const adminAuth = async (req, res, next) => {
             return next(new app_error_1.default(401, `Invalid token or session has expired`));
         }
         const user = await (0, user_service_1.findUniqueUser)({ id: JSON.parse(session).id });
-        console.log(user);
         if (user.role !== "admin")
             return next(new app_error_1.default(401, "Fobbitten route, you are not an admin"));
         if (!user) {
@@ -80,4 +79,40 @@ const adminAuth = async (req, res, next) => {
     }
 };
 exports.adminAuth = adminAuth;
+const adminAuthdel = async (req, res, next) => {
+    try {
+        let access_token;
+        if (req.headers.authorization &&
+            req.headers.authorization.startsWith('Bearer')) {
+            access_token = req.headers.authorization.split(' ')[1];
+        }
+        else if (req.cookies.access_token) {
+            access_token = req.cookies.access_token;
+        }
+        if (!access_token) {
+            return next(new app_error_1.default(401, 'You are not logged in'));
+        }
+        const decoded = (0, jwt_1.verifyJwt)(access_token, 'ab1234');
+        if (!decoded) {
+            return next(new app_error_1.default(401, `Invalid token or user doesn't exist`));
+        }
+        const session = await connect_redis_1.default.get(decoded.sub);
+        if (!session) {
+            return next(new app_error_1.default(401, `Invalid token or session has expired`));
+        }
+        const user = await (0, user_service_1.findUniqueUser)({ id: JSON.parse(session).id });
+        if (user.role !== "admin")
+            return next(new app_error_1.default(401, "Fobbitten route, you are not an admin"));
+        if (!user) {
+            return next(new app_error_1.default(401, `Invalid token or session has expired`));
+        }
+        res.locals.user = (0, lodash_1.omit)(user, user_service_1.excludedFields);
+        req.user = decoded;
+        next();
+    }
+    catch (err) {
+        next(err);
+    }
+};
+exports.adminAuthdel = adminAuthdel;
 //# sourceMappingURL=deserializeUser.js.map
