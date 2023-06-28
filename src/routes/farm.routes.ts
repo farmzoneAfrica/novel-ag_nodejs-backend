ad94feafedacb1fcb5e17e781d0386d15a1eb0a4
ushashir <laleoict@gmail.com>
2023-06-28 18:19:08 +0100

    removed middle ware for farmers

--- src/routes/farm.routes.ts
+++ src/routes/farm.routes.ts
@@ -24,10 +24,16 @@ import { validate } from '../middleware/validate';
 const farmRouter = express.Router();
 
 // farmRouter.post( '/:farmer_id', validate(createFarmSchema), auth, requireUser, createFarmHandler);
-farmRouter.post( '/:farmer_id', validate(createFarmSchema), auth, createFarmHandler);
-farmRouter.get( '/', auth, getFarmsHandler);
-farmRouter.get( '/:id', auth, getFarmHandler);
-farmRouter.patch( '/:id', validate(updateFarmSchema), auth, updateFarmHandler);
-farmRouter.delete( '/:id', auth, adminAuth, deleteFarmHandler);
+farmRouter.post( '/:farmer_id', validate(createFarmSchema), createFarmHandler);
+farmRouter.get( '/', getFarmsHandler);
+farmRouter.get( '/:id', getFarmHandler);
+farmRouter.patch( '/:id', validate(updateFarmSchema), updateFarmHandler);
+farmRouter.delete( '/:id', deleteFarmHandler);
+
+// farmRouter.post( '/:farmer_id', validate(createFarmSchema), auth, createFarmHandler);
+// farmRouter.get( '/', auth, getFarmsHandler);
+// farmRouter.get( '/:id', auth, getFarmHandler);
+// farmRouter.patch( '/:id', validate(updateFarmSchema), auth, updateFarmHandler);
+// farmRouter.delete( '/:id', auth, adminAuth, deleteFarmHandler);
 
 export default farmRouter;
